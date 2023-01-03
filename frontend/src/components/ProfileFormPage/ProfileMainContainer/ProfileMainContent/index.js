523fea871a4960353ebbff576130b2a5bc2b75b5
XueLan Wu <xuelanwu823@gmail.com>
2023-01-03 13:35:11 -0500

    clean code

--- frontend/src/components/ProfileFormPage/ProfileMainContainer/ProfileMainContent/index.js
+++ frontend/src/components/ProfileFormPage/ProfileMainContainer/ProfileMainContent/index.js
@@ -1,3 +1,4 @@
+import { getStorage } from "firebase/storage";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
